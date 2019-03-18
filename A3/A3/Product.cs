1ffb81c806bd2a70ad1368883d6736ee5bd4214e
Hadi Sheikhi <hadisheikhi77@gmail.com>
2019-03-18 12:38:32 +0330

    Products Class Completed

--- A3/A3/Product.cs
+++ A3/A3/Product.cs
@@ -7,11 +7,34 @@ namespace A3
 {
     public class Product
     {
-        public string Name;
-        public float Price;
-
+        public string _Name;
+        public float _Price;
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                _Name = value;
+            }
+        }
+        public float Price
+        {
+            get
+            {
+                return _Price;
+            }
+            set
+            {
+                _Price = value;
+            }
+        }
         public Product(string name, float price)
         {
+            Name = name;
+            Price = price;
         }
     }
 }
