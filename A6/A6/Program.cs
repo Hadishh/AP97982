40220ff256eb503daee4fd61827958473adb3017
Hadi Sheikhi <hadisheikhi77@gmail.com>
2019-04-17 13:46:37 +0430

    A6 Completed

---
+++ A6/A6/Program.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+namespace A6
+
+{
+    #region Structures
+    public struct TypeOfSize5
+    {
+        //5 Bytes
+        public byte byte1;
+        public byte byte2;
+        public byte byte3;
+        public byte byte4;
+        public byte byte5;
+        
+    }
+    public struct TypeOfSize8
+    {
+        TypeOfSize5 bytes5;
+        byte byte0;
+        byte byte1;
+        byte byte2;
+    }
+    public struct TypeOfSize16
+    {
+        TypeOfSize8 bytes8_0;
+        TypeOfSize8 bytes8_1;
+    }
+    public struct TypeOfSize22
+    {
+        TypeOfSize5 size1;
+        TypeOfSize5 size2;
+        TypeOfSize5 size3;
+        TypeOfSize5 size4;
+        byte byte1;
+        byte byte2;
+    }
+    public struct TypeOfSize32
+    {
+        TypeOfSize16 bytes16_0;
+        TypeOfSize16 bytes16_1;
+    }
+    public struct TypeOfSize64
+    {
+        TypeOfSize32 bytes32_0;
+        TypeOfSize32 bytes32_1;
+    }
+    public struct TypeOfSize125
+    {
+        //Bytes 110
+        TypeOfSize22 size1;
+        TypeOfSize22 size2;
+        TypeOfSize22 size3;
+        TypeOfSize22 size4;
+        TypeOfSize22 size5;
+        //15 Bytes
+        TypeOfSize5 size6;
+        TypeOfSize5 size7;
+        TypeOfSize5 size8;
+    }
+    public struct TypeOfSize128
+    {
+        TypeOfSize64 bytes64_0;
+        TypeOfSize64 bytes64_1;
+    }
+    public struct TypeOfSize256
+    {
+        TypeOfSize128 bytes_0;
+        TypeOfSize128 bytes_1;
+    }
+    public struct TypeOfSize512
+    {
+        TypeOfSize256 byte256_0;
+        TypeOfSize256 byte256_1;
+    }
+    public struct TypeOfSize1024
+    {
+        TypeOfSize512 bytes512_0;
+        TypeOfSize512 bytes512_1;
+    }
+    public struct TypeOfSize2048
+    {
+        TypeOfSize1024 bytes1024_0;
+        TypeOfSize1024 bytes1024_1;
+    }
+    public struct TypeOfSize4096
+    {
+        TypeOfSize2048 bytes2048_0;
+        TypeOfSize2048 bytes2048_1;
+    }
+    public struct TypeOfSize8192
+    {
+        TypeOfSize4096 bytes4096_0;
+        TypeOfSize4096 bytes4096_1;
+    }
+    public struct TypeOfSize16384
+    {
+        TypeOfSize8192 bytes8192_0;
+        TypeOfSize8192 bytes8192_1;
+    }
+    public struct TypeOfSize32768
+    {
+        TypeOfSize16384 bytes16384_0;
+        TypeOfSize16384 bytes16384_1;
+    }
+    public struct TypeOfSize65536
+    {
+        TypeOfSize32768 bytes32768_0;
+        TypeOfSize32768 bytes32768_1;
+    }
+    public struct TypeOfSize131072 {
+        TypeOfSize65536 bytes655536_0;
+        TypeOfSize65536 size2;
+    }
+    public struct TypeOfSize262144
+    {
+        TypeOfSize131072 size1;
+        TypeOfSize131072 size2;
+    }
+    public struct TypeOfSize512288
+    {
+        //512000 Bytes
+        TypeOfSize262144 kb256;
+        TypeOfSize131072 kb128;
+        TypeOfSize65536 kb65;
+        TypeOfSize32768 kb32;
+        TypeOfSize16384 kb16;
+        TypeOfSize4096 kb4;
+        //250 Bytes
+        TypeOfSize125 bytes125_0;
+        TypeOfSize125 bytes125_1;
+        //22 Bytes
+        TypeOfSize22 bytes22;
+        //15 Bytes
+        TypeOfSize5 bytes5_0;
+        TypeOfSize5 bytes5_1;
+        TypeOfSize5 bytes5_2;
+        //1 Byte
+        byte byte0;
+    }
+    #endregion
+
+    #region MaxStackOfDepthMethods
+    public struct TypeForMaxStackOfDepth10
+    {
+        TypeOfSize32768 kb32;
+        TypeOfSize8192 kb8;
+        TypeOfSize4096 kb4;
+    }
+    public struct TypeForMaxStackOfDepth100
+    {
+        TypeOfSize4096 kb4;
+        TypeOfSize512 bytes512;
+        TypeOfSize256 bytes256;
+        TypeOfSize125 size;
+        TypeOfSize22 size1;
+        TypeOfSize16 bytes16;
+    }
+    public struct TypeForMaxStackOfDepth1000
+    {
+        TypeOfSize256 bytes256;
+        TypeOfSize128 bytes128;
+        TypeOfSize32 bytes64;
+        TypeOfSize22 bytes22;
+        TypeOfSize16 bytes16;
+        sbyte b1;
+        sbyte b3;
+
+        //byte byte1;
+        //byte byte2;
+        //byte byte3;
+    }
+    public struct TypeForMaxStackOfDepth3000
+    {
+        TypeOfSize64 bytes64;
+        TypeOfSize32 bytes32;
+        TypeOfSize8 bytes8;
+        //TypeOfSize8 bytes8;
+        TypeOfSize5 b5;
+        byte c;
+        byte c2;
+        byte c1;
+    }
+    #endregion
+
+    #region Write On Heap Class
+    public class TypeWithMemoryOnHeap{
+        private List<List<int>> memoryList;
+        public void Allocate()
+        {
+            memoryList = new List<List<int>>();
+            for(int i = 0; i < 55000; i++)
+            {
+                List<int> mustAddedList = new List<int>
+                {
+                    1, 2, 3, 4, 5, 6, 7, 8
+                };
+                memoryList.Add(mustAddedList);
+            }
+        }
+        public void DeAllocate()
+        {
+            memoryList = null;
+        }
+    }
+    #endregion
+
+    #region Struct Or Class
+    public struct StructOrClass1
+    {
+        public int X;
+    }
+    public class StructOrClass2
+    {
+        public int X;
+    }
+    public struct StructOrClass3
+    {
+        public StructOrClass2 X;
+    }
+    #endregion
+    
+    public enum FutureHusbandType : int
+    {
+        None = 0,
+        IsBald = 1,
+        IsShort = 2,
+        HasBigNose = 4
+    }
+    
+    public class Program
+    {
+        public static int GetObjectType(object o)
+        {
+            if (o is string || o is null) return 0;
+            if (o is int[]) return 1;
+            if (o is int) return 2;
+            return -1;
+        }
+        public static bool IdealHusband(FutureHusbandType fht)
+        {
+            int allFutures = (int)((fht & FutureHusbandType.HasBigNose) | 
+                (fht & FutureHusbandType.IsBald) |
+                (fht & FutureHusbandType.IsShort));
+
+            if (allFutures == 2 || allFutures == 4 || 
+                allFutures == 0 || allFutures == 1)
+                return false;
+            if(allFutures == (int)(FutureHusbandType.IsBald | FutureHusbandType.IsShort) ||
+                allFutures == (int)(FutureHusbandType.IsBald | FutureHusbandType.HasBigNose)||
+                allFutures == (int)(FutureHusbandType.IsShort | FutureHusbandType.HasBigNose))
+                    return  true;
+            if (allFutures == 7)
+                return false;
+            return true;
+        }
+            
+        static void Main(string[] args)
+        {
+        }
+        
+    }
+}
---
+++ A6/A6Tests/MemoryTestTests.cs
@@ -0,0 +1,189 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using APAssignment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+using System.Runtime.InteropServices;
+using A6;
+//nameSpace Changed 
+namespace A6Tests
+{
+    [TestClass]
+    public class MemoryTestTests
+    {
+        [TestMethod]
+        public void VariableSizeTest()
+        {
+            //Structures Initialized
+            TypeOfSize5 tos5 = new TypeOfSize5();
+            VerifySize(tos5, 5);
+            TypeOfSize22 tos22 = new TypeOfSize22();
+            VerifySize(tos22, 22);
+            TypeOfSize125 tos125 = new TypeOfSize125();
+            VerifySize(tos125, 125);
+            TypeOfSize1024 tos1024 = new TypeOfSize1024();
+            VerifySize(tos1024, 1024);
+        }
+
+    
+        [TestMethod]
+        public void StackDepth10Test()
+        {
+           
+            int recursionDepth = GetMaxRecursion(0, new TypeForMaxStackOfDepth10());
+            Assert.AreEqual(10, recursionDepth);
+        }
+
+        [TestMethod]
+        public void StackDepth100Test()
+        {
+            int recursionDepth = GetMaxRecursion(0, new TypeForMaxStackOfDepth100());
+            Assert.AreEqual(100, recursionDepth);
+        }
+
+        [TestMethod]
+        public void StackDepth1000Test()
+        {
+            TypeOfSize2048 Kb2;
+            TypeOfSize128 bytes128;
+            int recursionDepth = GetMaxRecursion(0, new TypeForMaxStackOfDepth1000());
+            Assert.AreEqual(true, recursionDepth < 1003 && recursionDepth > 997);
+        }
+
+        [TestMethod]
+        public void StackDepth3000Test()
+        {
+            TypeOfSize8192 Kb8;
+            TypeOfSize2048 Kb2;
+            TypeOfSize512 bytes512;
+            TypeOfSize8 bytes8;
+            int recursionDepth = GetMaxRecursion(0, new TypeForMaxStackOfDepth3000());
+            Assert.AreEqual(true, recursionDepth <= 3005 && recursionDepth >= 2995);
+        }
+        
+        [TestMethod]
+        public void HeapMemoryTest()
+        {
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced, true);
+            var memSize = GC.GetTotalMemory(true);
+            TypeWithMemoryOnHeap r = new TypeWithMemoryOnHeap();
+            r.Allocate();
+
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced, true);
+            var memDiff1 = GC.GetTotalMemory(true) - memSize;
+            r.DeAllocate();
+
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced, true);
+            var memDiff2 = GC.GetTotalMemory(true) - memSize;
+
+            Assert.IsTrue(memDiff1 < 4_100_000 && memDiff1 > 3_900_000);
+            Assert.IsTrue(memDiff2 < 100_000 && memDiff2 > -100_000);
+        }
+        
+        [TestMethod]
+        public void RefValueTypeCopyTest1()
+        {
+            StructOrClass1 soc1a = new StructOrClass1();
+            soc1a.X = 1;
+            var soc1b = soc1a;
+            soc1a.X = 2; ;
+            Assert.AreNotEqual(soc1a.X, soc1b.X);
+        }
+        
+        [TestMethod]
+        public void RefValueTypeCopyTest2()
+        {
+            StructOrClass2 soc1a = new StructOrClass2();
+            soc1a.X = 1;
+            var soc1b = soc1a;
+            soc1a.X = 2; ;
+            Assert.AreEqual(soc1a.X, soc1b.X);
+        }
+    
+        [TestMethod]
+        public void RefValueTypeCopyTest3()
+        {
+            StructOrClass3 soc1a = new StructOrClass3();
+            var soc2 = new StructOrClass2();
+            soc2.X = 5;
+            soc1a.X = soc2;
+            var soc1b = soc1a;
+            soc1a.X.X = 6; 
+            Assert.AreEqual(soc1a.X.X, soc1b.X.X);
+            Assert.AreEqual(soc2.X, soc1b.X.X);
+
+        }
+        
+        [TestMethod]
+        public void BoxingTest()
+        {
+            StructOrClass1 soc = new StructOrClass1();
+            soc.X = 5;
+            object o = soc;
+            soc.X = 6;
+            StructOrClass1 soc2 = (StructOrClass1) o;
+            Assert.AreEqual(5, soc2.X);
+        }
+        
+        [TestMethod]
+        public void TypeTest()
+        {
+            string str = null;
+            int value = Program.GetObjectType(str);
+            Assert.AreEqual(0, value);
+            value = Program.GetObjectType(new int[] { 2 });
+            Assert.AreEqual(1, value);
+            value = Program.GetObjectType(24434);
+            Assert.AreEqual(2, value);
+        }
+        
+        [TestMethod]
+        public void IdealHusbandTest()
+        {
+            FutureHusbandType fht = FutureHusbandType.HasBigNose;
+            Assert.AreEqual(false, Program.IdealHusband(fht));
+
+            fht = FutureHusbandType.IsBald;
+            Assert.AreEqual(false, Program.IdealHusband(fht));
+
+            fht = FutureHusbandType.IsShort;
+            Assert.AreEqual(false, Program.IdealHusband(fht));
+
+            fht = FutureHusbandType.IsBald | FutureHusbandType.IsShort;
+            Assert.AreEqual(true, Program.IdealHusband(fht));
+
+            fht = FutureHusbandType.IsBald | FutureHusbandType.HasBigNose;
+            Assert.AreEqual(true, Program.IdealHusband(fht));
+
+            fht = FutureHusbandType.IsShort | FutureHusbandType.HasBigNose;
+            Assert.AreEqual(true, Program.IdealHusband(fht));
+
+            fht = FutureHusbandType.IsShort | FutureHusbandType.HasBigNose | FutureHusbandType.IsBald;
+            Assert.AreEqual(false, Program.IdealHusband(fht));
+
+
+        }
+        
+        #region Helper Methods
+        private void VerifySize<_Type>(_Type tos, int expectedSize)
+        {
+            int actualSize = Marshal.SizeOf(tos);
+            Assert.AreEqual(expectedSize, actualSize);
+        }
+
+        private int GetMaxRecursion<_Type>(int currentDepth, _Type bvt)
+        {
+            try
+            {
+                EnsureSufficientExecutionStack();
+                return GetMaxRecursion(currentDepth + 1, bvt);
+            }
+            catch (InsufficientExecutionStackException) {}
+            return currentDepth;
+        }
+        #endregion
+    }
+}
