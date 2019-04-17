8cfa11216b72fe25572199d690f0554b9ef6771e
Hadi Sheikhi <hadisheikhi77@gmail.com>
2019-04-18 00:17:18 +0430

    Change range of MaxStackDepth1000

--- A6/A6Tests/MemoryTestTests.cs
+++ A6/A6Tests/MemoryTestTests.cs
@@ -50,7 +50,7 @@ namespace A6Tests
             TypeOfSize2048 Kb2;
             TypeOfSize128 bytes128;
             int recursionDepth = GetMaxRecursion(0, new TypeForMaxStackOfDepth1000());
-            Assert.AreEqual(true, recursionDepth < 1005 && recursionDepth > 995);
+            Assert.AreEqual(true, recursionDepth < 1010 && recursionDepth > 990);
         }
 
         [TestMethod]
