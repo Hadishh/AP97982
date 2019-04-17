31e0bc71a465439ab4e320a17f438de2958ff6ea
Hadi Sheikhi <hadisheikhi77@gmail.com>
2019-04-17 23:27:00 +0430

    recursion Depth Tests Edited For Build Test on VSTS

--- A6/A6Tests/MemoryTestTests.cs
+++ A6/A6Tests/MemoryTestTests.cs
@@ -50,7 +50,8 @@ namespace A6Tests
             TypeOfSize2048 Kb2;
             TypeOfSize128 bytes128;
             int recursionDepth = GetMaxRecursion(0, new TypeForMaxStackOfDepth1000());
-            Assert.AreEqual(true, recursionDepth < 1010 && recursionDepth > 990);
+            Assert.AreEqual(1000, recursionDepth);
+            //Assert.AreEqual(true, recursionDepth < 1010 && recursionDepth > 990);
         }
 
         [TestMethod]
@@ -61,7 +62,8 @@ namespace A6Tests
             TypeOfSize512 bytes512;
             TypeOfSize8 bytes8;
             int recursionDepth = GetMaxRecursion(0, new TypeForMaxStackOfDepth3000());
-            Assert.AreEqual(true, recursionDepth <= 3020 && recursionDepth >= 2980);
+            Assert.AreEqual(3000, recursionDepth);
+            //Assert.AreEqual(true, recursionDepth <= 3020 && recursionDepth >= 2980);
         }
         
         [TestMethod]
