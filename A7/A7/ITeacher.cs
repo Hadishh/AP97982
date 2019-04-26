5f7fa4829da1c15c08dfaf4338f4b0e8aa1d05a9
Hadi Sheikhi <hadisheikhi77@gmail.com>
2019-04-26 23:04:11 +0430

    Dlete Unecessary comments

--- A7/A7/ITeacher.cs
+++ A7/A7/ITeacher.cs
@@ -2,7 +2,6 @@ namespace A7
 {
     public interface ITeacher
     {
-        // I think Name not needed because of ICitizen interface!
         string Name { get; }
         Degree TopDegree { get; set; }
         string ImgUrl { get; set; }
