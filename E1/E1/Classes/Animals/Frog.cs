2ed58af6c28824e3c362594909a686a6c443263e
Hadi Sheikhi <hadisheikhi77@gmail.com>
2019-04-29 13:42:23 +0430

    Frog Class Completed

--- E1/E1/Classes/Animals/Frog.cs
+++ E1/E1/Classes/Animals/Frog.cs
@@ -1,13 +1,69 @@
 ﻿using System;
+using E1.Enums;
 using E1.Interfaces;
 using Environment = E1.Enums.Environment;
 
 namespace E1.Classes.Animals
 {
-    public class Frog
+    public class Frog : IAnimal ,IWalkable, ISwimable
     {
         public Frog(string name, int age, double health, double speedRate)
         {
+            Name = name;
+            Age = age;
+            Health = health;
+            SpeedRate = speedRate;
+        }
+
+        public double SpeedRate { get; set; }
+
+        public string Name { get; }
+
+        public int Age { get; set; }
+
+        public double Health { get; set; }
+
+        public string EatFood()
+        {
+            string output = Name + " is a Frog and is eating";
+            return output;
+        }
+
+        public string Move(Environment moveType)
+        {
+            if (moveType == Environment.Land)
+            {
+                return this.Walk();
+            }
+            else if (moveType == Environment.Watery)
+            {
+                return this.Swim();
+            }
+            else
+            {
+                string output = Name + " is a Frog and can't move in " +
+                    moveType.ToString() + " environment";
+                return output;
+            }
+        }
+
+        public string Reproduction(IAnimal animal)
+        {
+            string output = this.Name + " is a Frog and reproductive with " +
+                animal.Name;
+            return output;
+        }
+
+        public string Swim()
+        {
+            string output = Name + " is a Frog and is swimming";
+            return output;
+        }
+
+        public string Walk()
+        {
+            string output = Name + " is a Frog and is walking";
+            return output;
         }
     }
 }
