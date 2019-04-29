079089defb4d4d090639e7ad6b49ecc45147241a
Hadi Sheikhi <hadisheikhi77@gmail.com>
2019-04-29 13:52:08 +0430

    Partridge Class Completed

--- E1/E1/Classes/Animals/Partridge.cs
+++ E1/E1/Classes/Animals/Partridge.cs
@@ -1,13 +1,69 @@
 ﻿using System;
+using E1.Enums;
 using E1.Interfaces;
 using Environment = E1.Enums.Environment;
 
 namespace E1.Classes.Animals
 {
-    public class Partridge
+    public class Partridge : IAnimal, IWalkable, IFlyable
     {
         public Partridge(string name, int age, double speedRate, double health)
         {
+            Name = name;
+            Age = age;
+            SpeedRate = speedRate;
+            Health = health;
+        }
+
+        public double SpeedRate { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public double Health { get; set; }
+
+        public string EatFood()
+        {
+            string output = Name + " is a Partridge and is eating";
+            return output;
+        }
+
+        public string Fly()
+        {
+            string output = Name + " is a Partridge and is flying";
+            return output;
+        }
+
+        public string Move(Environment moveType)
+        {
+            if (moveType == Environment.Land)
+            {
+                return this.Walk();
+            }
+            else if (moveType == Environment.Air)
+            {
+                return this.Fly();
+            }
+            else
+            {
+                string output = Name + " is a Partridge and can't move in " +
+                    moveType.ToString() + " environment";
+                return output;
+            }
+        }
+
+        public string Reproduction(IAnimal animal)
+        {
+            string output = this.Name + " is a Partridge and reproductive with " +
+                animal.Name;
+            return output;
+        }
+
+        public string Walk()
+        {
+            string output = Name + " is a Partridge and is walking";
+            return output;
         }
     }
 }
