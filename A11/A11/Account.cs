694c88ce248a319a059603f949effbaaaa99417f
Hadi Sheikhi <hadisheikhi77@gmail.com>
2019-05-23 17:27:19 +0430

    Accounts Calss Completed

---
+++ A11/A11/Account.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A11
+{
+    public class Account
+    {
+        public Account(double balance)
+        {
+            if (balance < 0)
+            {
+                Console.WriteLine("Initial balance is invalid. Setting balance to 0.");
+                Balance = 0;
+            }
+            else
+                Balance = balance;
+        }
+        public double Balance { get; set; }
+        public void Credit(double amount)
+        {
+            if (amount < 0)
+                throw new ArgumentException("Credit amount must be positive");
+            Balance += amount;
+        }
+        public bool Debit(double amount)
+        {
+            if (amount < 0)
+                throw new ArgumentException("Debit amount must be positive");
+            if (Balance - amount < 0)
+            {
+                Console.WriteLine("Debit amount exceeded account balance.");
+                return false;
+            }
+            else
+            {
+                Balance -= amount;
+                return true;
+            }
+        }
+    }
+}
