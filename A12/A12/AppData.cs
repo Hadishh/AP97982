607ffa32a32749c26e5841ea364fc086b4498b2b
Hadi Sheikhi <hadisheikhi77@gmail.com>
2019-05-29 00:41:18 +0430

    AppData Class Created

---
+++ A12/A12/AppData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A12
+{
+    public class AppData
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public double Rating { get; set; }
+        public long Reviews { get; set; }
+        public double Size { get; set; }
+    }
+}
