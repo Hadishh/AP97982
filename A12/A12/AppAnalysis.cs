6c911c35652c76a4f1ac23ff78754006be796683
Hadi Sheikhi <hadisheikhi77@gmail.com>
2019-05-29 17:36:13 +0430

    AppAnalysis NotImplemened

--- A12/A12/AppAnalysis.cs
+++ A12/A12/AppAnalysis.cs
@@ -8,5 +8,40 @@ namespace A12
 {
     public class AppAnalysis
     {
+        public List<AppData> Apps;
+        private AppAnalysis() {
+            Apps = new List<AppData>();
+        }
+        public static AppAnalysis AppAnalysisFactory(string appDataPath)
+        {
+            //TODO
+            return null;
+        }
+        public long AllAppsCount()
+        {
+            //TODO
+            return 0;
+        }
+        public long AppsAboveXRatingCount(double x)
+        {
+            //TODO
+            return 0;
+        }
+        public long RecentlyUpdatedCount(DateTime boundary)
+        {
+            //TODO
+            return 0;
+        }
+        public string RecentlyUpdatedFreqCat(DateTime boundry)
+        {
+            //TODO
+            return null;
+        }
+        public List<string> MostRatedGategories(double ratingBoundry, int n)
+        {
+            //TODO
+            return null;
+        }
+
     }
 }
