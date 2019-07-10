5ee3d9a2552110ef741edcca6ca5f19656e641b4
Hadi Sheikhi <hadisheikhi77@gmail.com>
2019-07-10 18:24:32 +0430

    Project Added

---
+++ P1/P1/EquationHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace P1
+{
+    class EquationHandler
+    {
+        private StackPanel MainStack { get; }
+        public List<TextBox> Equations { get; }
+        private List<Brush> AvailableColors = new List<Brush> {
+            Brushes.Black,
+            Brushes.Blue,
+            Brushes.Red,
+            Brushes.Green,
+            Brushes.Magenta
+        };
+        private int CurrentColorIndex;
+        public EquationHandler(StackPanel mainStack)
+        {
+            MainStack = mainStack;
+            AddEquation();
+            CurrentColorIndex = 1;
+        }
+        public void AddEquation()
+        {
+            TextBox newText = new TextBox();
+            newText.Background = AvailableColors[CurrentColorIndex];
+            MainStack.Children.Add(newText);
+            CurrentColorIndex = ++CurrentColorIndex > 4 ? 0 : CurrentColorIndex;
+        }
+    }
+}
---
+++ P1/P1/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace P1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool LeftMenuIsHidden = false;
+        EquationHandler EquationHandler;
+        public MainWindow()
+        {
+            InitializeComponent();
+            EquationHandler = new EquationHandler(EquationStack);
+        }
+
+        private void MenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!LeftMenuIsHidden)
+            {
+                Storyboard sb = Resources["CloseMenu"] as Storyboard;
+                MenuButton.Margin = new Thickness(8, 0, 0, 0);
+                LeftMenuIsHidden = true;
+                MenuButton.Content = ">>";
+                sb.Begin(DrawingPart);
+            }
+            else
+            {
+                Storyboard sb = Resources["OpenMenu"] as Storyboard;
+                MenuButton.Margin = new Thickness(0, 0, 0, 0);
+                sb.Begin(DrawingPart);
+                MenuButton.Content = "<<";
+                LeftMenuIsHidden = false;
+            }
+        }
+
+        private void ShowButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
