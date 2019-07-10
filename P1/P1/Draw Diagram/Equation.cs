9c27e1ca76f3a75a2679dbabd8785e81e53ab40f
Hadi Sheikhi <hadisheikhi77@gmail.com>
2019-07-11 02:41:55 +0430

    Equation Class Comlpeted

--- P1/P1/Draw Diagram/Equation.cs
+++ P1/P1/Draw Diagram/Equation.cs
@@ -6,11 +6,52 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.IO;
 
-namespace P1.Draw_Diagram
+namespace P1
 {
     public class Equation 
     {
+        public TextBox DataTextBox { get; set; }
+        public Button DeleteButton { get; set; }
+        public Equation()
+        {
+            DeleteButton = new Button();
+            DeleteButton.Margin = new Thickness(3);
+            DeleteButton.Content = new Image() {
+                Source = new BitmapImage(new Uri(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + @"\Images\CrossIcon.png", UriKind.RelativeOrAbsolute))
+            };
+            DataTextBox = new TextBox();
+            DataTextBox.Foreground = Brushes.Yellow;
+            DataTextBox.Margin = new Thickness(3);
+            DataTextBox.TextChanged += OnTextChanged_DrawEquation;
+            DataTextBox.TabIndex = 0;
+        }
 
+        public Grid GetGrid()
+        {
+            Grid classGrid = new Grid();
+            classGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(6, GridUnitType.Star) });
+            classGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+            Grid.SetColumn(DataTextBox, 0);
+            Grid.SetColumn(DeleteButton, 1);
+            classGrid.Children.Add(DataTextBox);
+            classGrid.Children.Add(DeleteButton);
+            return classGrid;
+        } 
+        public static List<string> GetSeparatedEquations(TextBox text)
+        {
+            List<string> results = new List<string>();
+            foreach (var item in text.Text.Split(new char[] { '+', '-' }))
+                results.Add(item.Replace(" ", string.Empty));
+            return results;
+        }
+        private void OnTextChanged_DrawEquation(object sender, TextChangedEventArgs e)
+        {
+            //Draw
+            if (sender is TextBox)
+                return;
+        }
     }
 }
--- P1/P1/Draw Diagram/EquationHandler.cs
+++ P1/P1/Draw Diagram/EquationHandler.cs
@@ -9,10 +9,10 @@ using System.Windows.Media;
 
 namespace P1
 {
-    class EquationHandler
+    public class EquationHandler
     {
         private StackPanel MainStack { get; }
-        public List<TextBox> Equations { get; }
+        public List<Equation> Equations { get; }
         private List<Brush> AvailableColors = new List<Brush> {
             new SolidColorBrush(Colors.Black) {Opacity = 0.5 },
             new SolidColorBrush(Colors.Blue) { Opacity = 0.5 },
@@ -24,22 +24,23 @@ namespace P1
         public EquationHandler(StackPanel mainStack)
         {
             MainStack = mainStack;
-            Equations = new List<TextBox>();
+            Equations = new List<Equation>();
             AddEquation();
             CurrentColorIndex = 1;
         }
+        /// <summary>
+        /// Creats an equation and add it to equation list
+        /// add Grid of Equation to stack panel
+        /// </summary>
         public void AddEquation()
         {
             Grid newGrid = new Grid();
             newGrid.ColumnDefinitions.Add(new ColumnDefinition());
-            TextBox newText = new TextBox();
-            newText.TextChanged += OnTextChanged; 
-            newText.Background = AvailableColors[CurrentColorIndex];
-
-            newText.Foreground = Brushes.Yellow;
-            newText.Margin = new Thickness(3);
-            MainStack.Children.Add(newText);
-            Equations.Add(newText);
+            Equation newEquation = new Equation();
+            newEquation.DataTextBox.TextChanged += OnTextChanged_AddEquation;
+            newEquation.DataTextBox.Background = AvailableColors[CurrentColorIndex];
+            MainStack.Children.Add(newEquation.GetGrid());
+            Equations.Add(newEquation);
             CurrentColorIndex = ++CurrentColorIndex >= AvailableColors.Count ? 0 : CurrentColorIndex;
         }
         /// <summary>
@@ -47,14 +48,9 @@ namespace P1
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void OnTextChanged(object sender, TextChangedEventArgs e)
+        private void OnTextChanged_AddEquation(object sender, TextChangedEventArgs e)
         {
-            if (sender is TextBox)
-            {
-                TextBox changedTextBox = sender as TextBox;
-                //Draw Equation
-            }
-            if (Equations.Last().Text != string.Empty)
+            if (Equations.Last().DataTextBox.Text != string.Empty)
                 AddEquation();
         }
 
