5d3acd866bb788c5202ab77074b49e251e14d69c
Hadi Sheikhi <hadisheikhi77@gmail.com>
2019-07-18 15:30:42 +0430

    Taylor Series Calculator Completed

---
+++ P1/P1/Taylor/TaylorEquation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace P1
+{
+    class TaylorEquation : Equation
+    {
+        public int N { get; private set; }
+        public double X0 { get; private set; }
+        TaylorSeries TaylorSeries { get; set; }
+        public TaylorEquation(int n, double x0, Func<double, double> function)
+        {
+            N = n;
+            X0 = x0;
+            Color = Brushes.Black;
+            TaylorSeries = new TaylorSeries(function);
+            Function = TaylorValue;
+        }
+        double TaylorValue(double x) => TaylorSeries.Calculate(N, X0, x);
+
+    }
+}
---
+++ P1/P1/Taylor/TaylorSeries.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1
+{
+    public class TaylorSeries
+    {
+        /// <summary>
+        /// Derivations List.
+        /// Note : Please Set Your Original Function At The First Element of List;
+        /// </summary>
+        private List<Func<double, double>> Derivations = new List<Func<double, double>>()
+        {
+            {Math.Sin },
+            {Math.Cos},
+            {(x) => -Math.Sin(x)},
+            {(x)=>-Math.Cos(x)}
+        };
+        public double CurrentValue { get; private set; }
+        private int Indexer;
+        Func<double, double> OriginalFunction { get; set; }
+        public TaylorSeries(Func<double, double> function)
+        {
+            CurrentValue = 1;
+            Indexer = 1;
+            OriginalFunction = function;
+        }
+        public double Calculate(int n, double x0,double x)
+        {
+            var func = Derivations[0];
+            double result = func(x0) * CurrentValue / Indexer;
+            if (func(x0) != 0)
+                n--;
+            func = Derivations[Indexer];
+            while(n > 0)
+            {
+                double value = func(x0) * CurrentValue * (x - x0) / Indexer;
+                if (func(x0) != 0)
+                    n--;
+                result += value;
+                CurrentValue = CurrentValue * (x - x0) / Indexer++;
+                func = Derivations[(Indexer) % 4];
+                double k = func(0);
+            }
+            CurrentValue = 1;
+            Indexer = 1;
+            return result;
+        }
+    }
+}
